37dd402a4546e16a7ca39478db2c9ce5641dd34a
Mike Leiter <light_cc@yahoo.com>
2020-07-29 23:17:22 -0400

    Clean up blank mainWindow create/close

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
+let mainWindow
+
 app.on('ready', _ => {
-    new BrowserWindow({
+    mainWindow = new BrowserWindow({
         height: 400,
         width: 400
+    })
+
+    mainWindow.on('closed', _ => {
+        console.log('closed!')
+        mainWindow = null
     })
 })

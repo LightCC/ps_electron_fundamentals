5ceff9e051cd222a8d180b0a1dd791c5ca73a08e
Mike Leiter <light_cc@yahoo.com>
2020-08-08 23:17:03 -0400

    Generate 3 separate windows instead of just 1.

--- src/main.js
+++ src/main.js
@@ -6,30 +6,36 @@ const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const ipc = electron.ipcMain
 
-let mainWindow
+const windows = []
 
 app.on('ready', _ => {
-    mainWindow = new BrowserWindow({
-        title: "My Electron App",
-        backgroundColor: '#FFF',
-        height: 400,
-        width: 400,
-        center: true,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
+    [1, 2, 3].forEach(i => {
+        let win = new BrowserWindow({
+            title: `My Electron App (${i})`,
+            backgroundColor: '#FFF',
+            height: 400,
+            width: 400,
+            center: true,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        })
 
-    mainWindow.loadURL(`file://${__dirname}/countdown.html`)
+        win.loadURL(`file://${__dirname}/countdown.html`)
 
-    mainWindow.on('closed', _ => {
-        console.log('closed!')
-        mainWindow = null
+        win.on('closed', _ => {
+            console.log(`closed window #${i}!`)
+            win = null
+        })
+
+        windows.push(win)
     })
 })
 
 ipc.on('countdown-start', _ => {
     countdown(count => {
-        mainWindow.webContents.send('countdown', count)
+        windows.forEach(win => {
+            win.webContents.send('countdown', count)
+        })
     })
 })
